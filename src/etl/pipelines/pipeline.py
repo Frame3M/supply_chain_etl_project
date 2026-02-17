4d7855082f8e19919fd8ae4ca8f9b1a9ea0c4d52
Frame <ortegamarco.it@gmail.com>
2026-02-16 23:07:21 -0300

    Se agrego exception en el pipeline y se creo un DER para la base de datos

--- src/etl/pipelines/pipeline.py
+++ src/etl/pipelines/pipeline.py
@@ -17,23 +17,28 @@ def run_pipeline():
     log_header(logger)
     logger.info("Pipeline Start")
     
-    # Extract data
-    df_raw = extract_from_csv(path= '../../../data/raw/supply_chain_raw.csv', enconding= 'cp1252')
-    
-    # Transformations
-    df = standarize_columns(df_raw)
-    df = clean_strings(df)
-    df = fix_dtypes(df)
-    df = remove_duplicates(df)
-    df = drop_rows_with_nulls(df, subset= ['customer_lname', 'customer_zipcode'])
-    df = fill_missing_zipcode(df)
-    
-    # Validations
-    validate_not_null(df, columns= ['category_id', 'customer_id', 'department_id', 'order_customer_id', 'order_id', 'order_item_cardprod_id', 'order_item_id', 'product_card_id', 'product_category_id'])
-    validate_item_quantity_not_zero(df)
-    validate_non_negative_columns(df, except_cols= ['benefit_per_order', 'latitude', 'longitude', 'order_item_profit_ratio', 'order_profit_per_order'])
-    validate_date_order(df, 'order_date_dateorders', 'shipping_date_dateorders')
-    validate_not_future_date(df)
-    
-    # Save
-    save_to_csv(df, save_path= '../../../data/silver/supply_chain_silver.csv')
+    try:
+        # Extract data
+        df_raw = extract_from_csv(path= '../../../data/raw/supply_chain_raw.csv', enconding= 'cp1252')
+        
+        # Transformations
+        df = standarize_columns(df_raw)
+        df = clean_strings(df)
+        df = fix_dtypes(df)
+        df = remove_duplicates(df)
+        df = drop_rows_with_nulls(df, subset= ['customer_lname', 'customer_zipcode'])
+        df = fill_missing_zipcode(df)
+        
+        # Validations
+        validate_not_null(df, columns= ['category_id', 'customer_id', 'department_id', 'order_customer_id', 'order_id', 'order_item_cardprod_id', 'order_item_id', 'product_card_id', 'product_category_id'])
+        validate_item_quantity_not_zero(df)
+        validate_non_negative_columns(df, except_cols= ['benefit_per_order', 'latitude', 'longitude', 'order_item_profit_ratio', 'order_profit_per_order'])
+        validate_date_order(df, 'order_date_dateorders', 'shipping_date_dateorders')
+        validate_not_future_date(df)
+        
+        # Save
+        save_to_csv(df, save_path= '../../../data/silver/supply_chain_silver.csv')
+        
+    except Exception as e:
+        logger.error("Execution stopped because of an error", exc_info=True)
+        raise
